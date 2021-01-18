98e47d86a4204026be611856d46970ae9b028503
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-18 05:10:10 -0800

    add react rputer dom

--- src/Components/Useeffect.jsx
+++ src/Components/Useeffect.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect } from 'react'
+import Axios from 'axios'
 
 const Useeffect = () => {
 useEffect(()=>{
-    console.log('hello')
+    const lol= async()=>{
+        await Axios({
+            method:'get',
+            url:'http://127.0.0.1:8000/api/product/'
+        }).then(response=>{
+            console.log(response.data);
+        })
+    }
+    lol()
+
 },[])
     return (
         <div>
