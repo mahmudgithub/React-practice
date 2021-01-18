ee94d6dc65f54305664d07293662e3818f7eda67
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-17 19:04:35 -0800

    edit something

--- src/Components/First.jsx
+++ src/Components/First.jsx
@@ -3,15 +3,15 @@ import { Second } from './Second'
 
 export const First = () => {
     const [x, setX] = useState(0)
-    // const [y, setY] = useState(0)
+   
 
     return (
         <div>
-            {/* <input onChange={(e)=>setX(x+1)} type='number'/> */}
+            
             <button onClick={(e)=>setX(x+1)} type='number'>+</button>
             <h1>{x}</h1> 
             <button onClick={(e)=>setX(x-1)} type='number'>-</button>
-            {/* <input onChange={(e)=>setX(x-1)} type="number"/> */}
+            
             
             <Second/>
         </div>
