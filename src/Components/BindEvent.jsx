8808386b0724092f28eab9e43c2bf04ade53888b
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-04 10:36:30 -0800

    use setState and envet

--- src/Components/BindEvent.jsx
+++ src/Components/BindEvent.jsx
@@ -32,20 +32,68 @@
 
 // export default BindEvent
 
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export class BindEvent extends Component {
-    lol=(a)=>{
-        alert(a)
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={this.lol.bind(this,'nowsin')}>click</button>
-            </div>
-        )
-    }
-}
+// export class BindEvent extends Component {
+//     lol=(a)=>{
+//         alert(a)
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.lol.bind(this,'nowsin')}>click</button>
+//             </div>
+//         )
+//     }
+// }
 
-export default BindEvent
+// export default BindEvent
 
+// import React, { Component } from 'react'
+
+// export class BindEvent extends Component {
+//     lol=(a)=>{
+//         alert(a)
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.lol.bind(this,'hello world')}>click</button>
+//             </div>
+//         )
+//     }
+// }
+
+// export default BindEvent
+
+// import React, { Component } from 'react'
+
+// export class BindEvent extends Component {
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+//              name:'mamud hosain'
+//         }
+
+//     }
+
+            
+//     lol=()=>{
+//         this.setState({
+//             name:'fariha Lmyaa'
+//         })
+//     }
+
+
+//     render() {
+//         return (
+//             <div>
+//                 <p>{this.state.name}</p>
+//                 <button onClick={this.lol}>click </button>
+//             </div>
+//         )
+//     }
+// }
+
+// export default BindEvent
