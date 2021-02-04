e543b48a3e0e7d4d072cbca1ec57ce416112c579
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-04 10:01:18 -0800

    use bind in class

--- src/Components/BindEvent.jsx
+++ src/Components/BindEvent.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+// import React from 'react'
 
-const BindEvent = () => {
-    const lol=(a)=>{
+// const BindEvent = () => {
+//     const lol=(a)=>{
+//         alert(a)
+//     }
+//     return (
+//         <div>
+//             <button onClick={lol.bind(this,'hello world')}>click</button>
+//         </div>
+//     )
+// }
+
+// export default BindEvent
+
+
+
+import React, { Component } from 'react'
+
+export class BindEvent extends Component {
+    lol=(a)=>{
         alert(a)
     }
-    return (
-        <div>
-            <button onClick={lol.bind(this,'hello world')}>click</button>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <button onClick={this.lol.bind(this,'mahmud')}>click</button>
+            </div>
+        )
+    }
 }
 
 export default BindEvent
