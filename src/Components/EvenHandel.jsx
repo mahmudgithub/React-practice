3c29fb176841a56d845387b24acdb7f339461847
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-04 09:02:44 -0800

    event handeling way two

--- src/Components/EvenHandel.jsx
+++ src/Components/EvenHandel.jsx
@@ -1,13 +1,58 @@
+// import React from 'react'
+
+// const EvenHandel = () => {
+//     const change=()=>{
+//         console.log('hello')
+        
+//     }
+//     return (
+//         <div>
+//         <button onClick={change}>Click me</button>
+//         </div>
+//     )
+// }
+
+// export default EvenHandel
+
+// import React from 'react'
+
+// const EvenHandel = () => {
+//     const change=()=>{
+//         console.log('hello')
+//     }
+//     return (
+//         <div>
+//             <button onClick ={change}>click</button>
+//         </div>
+//     )
+// }
+
+// export default EvenHandel
+
+// import React from 'react'
+
+// const EvenHandel = () => {
+//     const change=()=>{
+//         console.log('add')
+//     }
+//     return (
+//         <div>
+//          <button onClick={change}>click</button>   
+//         </div>
+//     )
+// }
+
+// export default EvenHandel
+
 import React from 'react'
 
 const EvenHandel = () => {
-    let change=()=>{
+    const lol=()=>{
         console.log('hello')
-        
     }
     return (
         <div>
-        <button onClick={change}>Click me</button>
+            <button onClick={()=>lol()}>click</button>
         </div>
     )
 }
