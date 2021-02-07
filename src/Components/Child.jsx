0ce69c01f1748af238865911dc8989328d8533ed
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-07 03:36:07 -0800

    arrary value pass to child props

--- src/Components/Child.jsx
+++ src/Components/Child.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+// import React from 'react'
 
-const Child = (props) => {
-    return (
-        <div>
-            {props.lol} ,how are you
-        </div>
-    )
-}
+// const Child = (props) => {
+//     return (
+//         <div>
+//             {props.lol} ,how are you
+//         </div>
+//     )
+// }
 
-export default Child
+// export default Child
+
+
+// import React from 'react'
+
+// function Child(props) {
+//     return (
+//         <div>
+//             {props.array}
+//             <br/>
+//             {props.array[2]}
+//         </div>
+//     )
+// }
+
+// export default Child
--- src/Components/Mother.jsx
+++ src/Components/Mother.jsx
@@ -1,23 +1,47 @@
-import React, { Component } from 'react'
-import Child from './Child'
+// import React, { Component } from 'react'
+// import Child from './Child'
 
-export class Mother extends Component {
-    constructor(props) {
-        super(props)
+// export class Mother extends Component {
+//     constructor(props) {
+//         super(props)
     
-        this.state = {
-             message:'hi mahmd'
-        }
-    }
+//         this.state = {
+//              message:'hi mahmd'
+//         }
+//     }
 
     
-    render() {
-        return (
-            <div>
-                <Child lol={this.state.message}/>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <Child lol={this.state.message}/>
+//             </div>
+//         )
+//     }
+// }
 
-export default Mother
+// export default Mother
+
+
+// import React, { Component } from 'react'
+// import Child from './Child'
+
+// export class Mother extends Component {
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+//              names:['mahmud','hossain','lamyaa','nowsin']
+//         }
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 <Child array={this.state.names}/>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Mother
