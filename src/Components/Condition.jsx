1f9b743b0311d5d7217c62b21a47a047378366c7
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-07 06:53:26 -0800

    use element variable

--- src/Components/Condition.jsx
+++ src/Components/Condition.jsx
@@ -16,3 +16,28 @@
 //     }
 // }
 // export default Condition
+
+// // second way element variable
+// import React, { Component } from 'react'
+
+// export class Condition extends Component {
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+//              isuser:false
+//         }
+//     }
+    
+//     render() {
+//         let msg;
+//         if(this.state.isuser){
+//             msg=<div>i am user</div>
+//         }else{
+//             msg=<div>i am not user</div>
+//         }
+//         return(msg)
+//     }
+// }
+
+// export default Condition
