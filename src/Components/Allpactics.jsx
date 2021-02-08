a4ad33f7cb57bb22a999983dcab1045abf98a52e
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-07 19:26:39 -0800

    state as a variamble

--- src/Components/Allpactics.jsx
+++ src/Components/Allpactics.jsx
@@ -22,3 +22,28 @@
 // }
 
 // export default Allpactics
+
+// import React, { Component } from 'react'
+
+// export class Allpactics extends Component {
+//     constructor(props) {
+//         super(props)
+//         const lol={
+//             name:'mahmud',
+//             city:['rajshahi','dhaka','kulna']
+//         }
+    
+//         this.state = lol
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 {this.state.name}
+//                 {this.state.city[1]}
+//             </div>
+//         )
+//     }
+// }
+
+// export default Allpactics
