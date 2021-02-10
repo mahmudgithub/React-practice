43cee5716efb8125cd41061c0f73286fdf0460b0
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-10 02:34:07 -0800

    second way of use react fragment

--- src/Components/Fragments.jsx
+++ src/Components/Fragments.jsx
@@ -1,15 +1,31 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+
+// export class Fragments extends Component {
+//     render() {
+//         return (
+//             <React.Fragment>
+
+//                 <h1>i am h1</h1>
+//                 <p>i am paragram</p>
+//             </React.Fragment>
+               
+        
+//         )
+//     }
+// }
+
+// export default Fragments
+
+import React, { Component,Fragment } from 'react'
 
 export class Fragments extends Component {
     render() {
         return (
-            <React.Fragment>
 
-                <h1>i am h1</h1>
-                <p>i am paragram</p>
-            </React.Fragment>
-               
-        
+            <Fragment>
+                <h1>hello</h1>
+                <p>i am paragraph </p>
+            </Fragment>
         )
     }
 }
