ae5c91dadaf24b877d1847906f1ecd1f67cc3baf
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-10 02:30:28 -0800

    first way of use react fragment

--- src/Components/Fragments.jsx
+++ src/Components/Fragments.jsx
@@ -3,9 +3,13 @@ import React, { Component } from 'react'
 export class Fragments extends Component {
     render() {
         return (
-            <div>
-                hello
-            </div>
+            <React.Fragment>
+
+                <h1>i am h1</h1>
+                <p>i am paragram</p>
+            </React.Fragment>
+               
+        
         )
     }
 }
