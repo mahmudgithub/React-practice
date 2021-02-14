9e3b8050e84b30bb31471f89e2b15eac3e0b6cbb
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-13 19:45:25 -0800

    condition base useEffect

--- src/Components/HookuseEffect.jsx
+++ src/Components/HookuseEffect.jsx
@@ -14,3 +14,64 @@
 // }
 
 // export default HookuseEffect
+
+
+// import React,{useState,useEffect} from 'react'
+
+// const HookuseEffect = () => {
+//     const [count, setcount] = useState('mahmud')
+//     useEffect(()=>{
+//         document.title= `hi:${count}`
+//     })
+//     return (
+//         <div>
+//             {count}
+//             <br/>
+//             <button onClick={()=>setcount('hossain')}>click to chnage</button>
+//         </div>
+//     )
+// }
+
+// export default HookuseEffect
+
+
+// import React,{useState,useEffect} from 'react'
+
+// const HookuseEffect = () => {
+//     const [change, setchange] = useState('type anything')
+//     useEffect(()=>{
+//         document.title=`type: ${change}`
+//     })
+//     return (
+//         <div>
+//             {change}
+//             <br/>
+//             <input value={change} onChange={(e)=>setchange(e.target.value)} type="text"/>
+//         </div>
+//     )
+// }
+
+// export default HookuseEffect
+
+
+import React,{useState,useEffect} from 'react'
+
+const HookuseEffect = () => {
+    const [count, setcount] = useState(0)
+    const [names, setnames] = useState('')
+    useEffect(()=>{
+        document.title=`count:${count}`
+        console.log(`Effect run`);
+    },[names])
+    return (
+        <div>
+            <h1>my name is:{names}</h1>
+            <input value={names} onChange={(e)=>setnames(e.target.value)}ype="text"/>
+            <br/>
+            vaue:{count}<br/>
+            <button onClick={()=>setcount(count+1)}>incrise values </button>
+        </div>
+    )
+}
+
+export default HookuseEffect
