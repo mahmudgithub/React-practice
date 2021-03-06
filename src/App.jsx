ac43d548e68174d9f8abf54a5fa21070a3841340
mahmudgithub <mahmudhossain838@gmail.com>
2021-03-06 10:02:35 +0000

    pactics router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import Axioss from './Components/Axioss';
 import Propss from './Components/Propss';
 import Reactfragment from './Components/Reactfragment';
 import Useeffect from './Components/Useeffect';
-import {BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+// import {BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Router1 from './Components/Router1';
 import Router2 from './Components/Router2';
 import Httpcompl1 from './Components/Httpcompl1';
@@ -22,35 +22,71 @@ import Higherorder from './Components/Higherorder';
 import Higherorder2 from './Components/Higherorder2';
 import HookuseState from './Components/HookuseState';
 import HookuseEffect from './Components/HookuseEffect';
+// import Home from './Components/Home';
+// import Base from './Components/Base';
+// import About from './Components/About';
 
+// const App=()=>{
+// return (
+//   <BrowserRouter>
+// <ul>
+//   <li>
+//    <Link to ='/'>home</Link>
+//   </li>
+//   <li>
+//    <Link to ='/Base'>base</Link>
+//   </li>
+//   <li>
+//    <Link to ='/About'>about</Link>
+//   </li>
+// </ul>
+// <Switch>
+//   <Route exact path='/'><Home/></Route>
+//   <Route exact path='/Base'><Base/> </Route>
+//   <Route exact path='/About'><About/> </Route>
+// </Switch>
+// </BrowserRouter>
+// )}
+// export default App;
 
+import{BrowserRouter,Route,Switch,Link} from 'react-router-dom';
+import Home from './Components/Home'
+import About from './Components/About'
+import Base from './Components/Base'
 
-const App=()=>{
-return (
-<div>
-{/* <BrowserRouter>
+const App = () => {
+  return (
+<BrowserRouter>
 <ul>
   <li>
-    <Link to='/router1'> router1 </Link>
+    <Link to='/' >Home</Link>
   </li>
   <li>
-    <Link to='/Router2'> router2 </Link>
-  </li> 
+    <Link to='/About' >ABOUT</Link>
+  </li>
+  <li>
+    <Link to='/Base' >BASE</Link>
+  </li>
 </ul>
-<Switch>
-  <Route path='/Router1'>
-    <Router1/>
-  </Route>
 
-  <Route path='/Router2'>
-    <Router2/>
-  </Route>
-</Switch>
-</BrowserRouter> */}
+  <Switch>
+    <Route exact path='/'><Home/></Route>
+    <Route exact path='/About'><About/></Route>
+    <Route exact path='/Base'><Base/></Route>
+
+  </Switch>
+</BrowserRouter>
+  )
+}
+
+export default App
 
 
 
 
+// const App = () => {
+  // return (
+    // <div>
 {/* <Reactfragment/> */}
 {/* <Useeffect/>  */}
 {/* <Httpcompl1/> */}
@@ -70,10 +106,10 @@ return (
 {/* <HookuseState/> */}
 {/* <HookuseEffect/> */}
 
+{/* <Allpactics name='mahmud' city='rajshahi'> i am child of props</Allpactics> */}
+    // </div>
+  // )
+// }
 
+// export default App
 
-<Allpactics name='mahmud' city='rajshahi'> i am child of props</Allpactics>
-</div> 
-  )
-}
-export default App;
---
+++ src/Components/Base.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Base = () => {
+    return (
+        <div>
+            <h1>base</h1>
+        </div>
+    )
+}
+
+export default Base
